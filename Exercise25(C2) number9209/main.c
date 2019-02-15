8f43ce744f94d99cfc5091dece6193eeb398c7dc
NikitaLapin <47674910+NikitaLapin@users.noreply.github.com>
2019-02-15 23:07:41 +0300

    Add files via upload

--- Exercise25(C2) number9209/main.c
+++ Exercise25(C2) number9209/main.c
@@ -3,6 +3,41 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int a;
+    printf("How many elements do you want to see in your array?\n");
+    scanf("%d", &a);
+    int array[a];
+
+    for(int i = 0; i < a; i++){
+        scanf("%d ", &array[i]);
+        if(array[i] < 0){
+            printf("This number is incorrect. Enter number bigger than 0\n");
+            i--;
+        }
+    }
+
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        sum += array[i];
+    }
+
+    int count = 0;
+    if(sum % 2 == 0){
+        for(int i = 0; i < a; i++){
+            if(array[i] % 2 == 0){
+                count++;
+            }
+        }
+        printf("Number of countable numbers : %d\n", count);
+    }
+
+    if(sum % 2 != 0){
+        for(int i = 0; i < a; i++){
+            if(array[i] % 2 != 0){
+                count++;
+            }
+        }
+        printf("Number of uncountable numbers : %d\n", count);
+    }
     return 0;
 }
